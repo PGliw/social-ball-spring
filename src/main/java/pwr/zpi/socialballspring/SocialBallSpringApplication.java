ea780d6a5133c64e8a728bc7eae63c015a89717a
Piotr Chorościn <titkot96@gmail.com>
2020-04-03 02:37:40 +0200

    Initial commit, connect to MySQL DB

---
+++ src/main/java/pwr/zpi/socialballspring/SocialBallSpringApplication.java
@@ -0,0 +1,13 @@
+package pwr.zpi.socialballspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBallSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBallSpringApplication.class, args);
+	}
+
+}
